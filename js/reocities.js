7c1f257aa9e166f5cb77e04f3ff05a9da3373d40
Matthew Ackard <Matthew Ackard>
2016-05-30 12:27:37 -0700

    new json directories and a few changes to images on the page

--- js/reocities.js
+++ js/reocities.js
@@ -9,11 +9,33 @@ var subDirectories =  [
                         "CapeCanaveral",
                         "CapitolHill",
                         "CollegePark",
-                        "Tokyo"
+                        "Dimension",
+                        "Dreamworld",
+                        "Dungeon",
+                        "Labyrinth",
+                        "Shadowlands",
+                        "Tokyo",
+                        "Underworld",
+                        "Zone"
                       ];
 var siteArray = [];
 var neighborhood = "";
 var randNum;
+
+function pickSite() {
+   if (neighborhood == "") {                                       //shows an alert if no subdirectory was selected
+    $(".select").css("border","2px solid red");
+    alert("You must select a subdirectory");
+  }
+  else {
+    $.getJSON("../js/json/reocities/" + neighborhood + ".json", function(data) {              //when button is clicked the json file is selected
+      neighborhood = data.subDir;                                                               //based on the neighborhood and loaded. the addresses 
+      siteArray = data.address;                                                                 //for the pages are loaded into an array and                                                  
+      randNum = Math.floor(Math.random() * siteArray.length);                                    //randomized. the user is then redirected to the page
+      window.location = "http://reocities.com/"+neighborhood+"/"+siteArray[randNum];
+    })
+  }
+}
 
 for (var i = 0; i < subDirectories.length; i++) {                   //creates a list of options from the subDirectories array
   $("#directorySelect").append("<option value='" + subDirectories[i] + "'>" + subDirectories[i] + "</option>");
@@ -21,24 +43,11 @@ for (var i = 0; i < subDirectories.length; i++) {                   //creates a 
 
 $("select").change(function() {                                     //sets neighborhood to the the selected subdirectory
   neighborhood = $("select option:selected").attr("value");
-  console.log(neighborhood);
 })
 
-$("#submit").click(function() { 
-  if (neighborhood == "") {                                       //shows an alert if no subdirectory was selected
-    $(".select").css("border","2px solid red");
-    alert("You must select a subdirectory");
-  }
-  else {
-    $.getJSON("../js/json/reocities/" + neighborhood + ".json", function(data) {              //when button is clicked the json file is selected
-      neighborhood = data.subDir;
-      console.log(neighborhood);                                                                //based on the neighborhood and loaded. the addresses
-                                                                                                //for the pages are loaded into an array and
-                                                                                               //randomized. the user is then redirected to the page
-      siteArray = data.address;  
-      console.log(siteArray);
-      randNum = Math.floor(Math.random() * siteArray.length);
-      window.location = "http://reocities.com/"+neighborhood+"/"+siteArray[randNum];
-    })
-  }
+$("#submit").click(pickSite);
+$("#randomSub").click(function() {
+  randNum = Math.floor(Math.random() * subDirectories.length);
+  neighborhood = subDirectories[randNum];
+  pickSite();
 });
